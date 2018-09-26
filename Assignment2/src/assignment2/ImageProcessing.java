980893be087f8bf42064f3a43e6597b0a763be41
MaksymD2099 <43325608+MaksymD2099@users.noreply.github.com>
2018-09-26 07:53:16 -0400

    Add files via upload

--- Assignment2/src/assignment2/ImageProcessing.java
+++ Assignment2/src/assignment2/ImageProcessing.java
@@ -6,11 +6,25 @@ import java.awt.image.BufferedImage;
 
 public class ImageProcessing extends ImageProcessingGUI
 {
-    public ImageProcessing(){     
-            
-    if(ConvertGreyScaleCheck.isSelected()) {
-     final BufferedImage input = InputImagePanel.getBufferedImage();     
-     BufferedImage output = new BufferedImage(input.getWidth(),
+   // public final BufferedImage input = InputImagePanel.getBufferedImage();    
+    
+    public ImageProcessing()
+    {
+        
+    }
+      
+    public static void applyGreyScale(BufferedImage input,BufferedImage output){
+          
+        /*if (checkbox.isSelected())
+        {
+            input = ImageProcessing.applySomeFiler(input);
+        }
+        
+        if (otherChecboc.)
+        {
+            input = 
+        }*/       
+       output = new BufferedImage(input.getWidth(),
                                                  input.getHeight(),
                                                  input.getType());
         
@@ -30,16 +44,88 @@ public class ImageProcessing extends ImageProcessingGUI
                 // Write the grey scales color to output pixel
                 output.setRGB(i, j, new Color(avg, avg, avg).getRGB());
             }
+        }        
+         
+        OutputImagePanel.setImage(output);        
+    }
+    
+   public static BufferedImage applyGaussianBlur(BufferedImage input)
+    {
+        final float gaussianKernel[][] = new float[][]
+        {
+            {0.00000067f, 0.00002292f, 0.00019117f, 0.00038771f, 0.00019117f, 0.00002292f, 0.00000067f},
+            {0.00002292f, 0.00078634f, 0.00655965f, 0.01330373f, 0.00655965f, 0.00078633f, 0.00002292f},
+            {0.00019117f, 0.00655965f, 0.05472157f, 0.11098164f, 0.05472157f, 0.00655965f, 0.00019117f},
+            {0.00038771f, 0.01330373f, 0.11098164f, 0.22508352f, 0.11098164f, 0.01330373f, 0.00038771f},
+            {0.00019117f, 0.00655965f, 0.05472157f, 0.11098164f, 0.05472157f, 0.00655965f, 0.00019117f},
+            {0.00002292f, 0.00078634f, 0.00655965f, 0.01330373f, 0.00655965f, 0.00078633f, 0.00002292f},
+            {0.00000067f, 0.00002292f, 0.00019117f, 0.00038771f, 0.00019117f, 0.00002292f, 0.00000067f}
+        };
+
+        return applyConvolutionFilter(input, gaussianKernel);
+    }
+   
+   private static BufferedImage applyConvolutionFilter(BufferedImage input, float[][] kernel)
+    {
+        BufferedImage output = new BufferedImage(input.getWidth(), input.getHeight(), input.getType());
+        
+        // Set output image from input image (img)
+        for (int j=0; j<input.getHeight(); ++j)
+        {
+            for (int i=0; i<input.getWidth(); ++i)
+            {
+                // Calculate the gaussian blur
+                Color pixelColor = applyKernel(input, kernel, i, j);
+                output.setRGB(i, j, pixelColor.getRGB());                
+            }
         }
         
-        OutputImagePanel.setImage(output);
-     }
-                      
-}
+        return output;
+    }
 
-    public static void main(String[] args) {
+   
+    private static Color applyKernel(BufferedImage img, float[][] kernel, int row, int column)
+    {
+        float red = 0.0f;
+        float green = 0.0f;
+        float blue = 0.0f;
 
+        int minIndexH = -(kernel.length / 2);
+        int maxIndexH = minIndexH + kernel.length;
+        int minIndexV = -(kernel[0].length / 2);
+        int maxIndexV = minIndexV + kernel[0].length;
+        
+        for (int i = minIndexH; i < maxIndexH; ++i)
+        {
+            for (int j=minIndexV; j<maxIndexV; ++j)
+            {
+                int indexH = wrapHorizontalIndex(img, row + i);
+                int indexV = wrapVerticalIndex(img, column + j);
+                Color col = new Color(img.getRGB(indexH, indexV));
 
+                red += col.getRed() * kernel[i-minIndexH][j-minIndexV];
+                green += col.getGreen() * kernel[i-minIndexH][j-minIndexV];
+                blue += col.getBlue() * kernel[i-minIndexH][j-minIndexV];
+            }
+        }
+
+        red = Math.min(Math.max(red, 0.0f), 255.0f);
+        green = Math.min(Math.max(green, 0.0f), 255.0f);
+        blue = Math.min(Math.max(blue, 0.0f), 255.0f);
+        
+        return new Color((int) red, (int) green, (int) blue);
+    }
+    
+    private static int wrapHorizontalIndex(BufferedImage input, int i)
+    {
+        // This takes care of negative and positive indices beyond the image resolution
+        return (i + input.getWidth()) % input.getWidth();
+    }
+
+    private static int wrapVerticalIndex(BufferedImage input, int i)
+    {
+        // This takes care of negative and positive indices beyond the image resolution
+        return (i + input.getHeight()) % input.getHeight();
     }
     
 }
